c298288208f073ba8e01f499340e5c06ec420c48
Ilja Rotar <kontakt@ilja-rotar.de>
2023-11-02 16:27:33 +0100

    go back to kubebuilder

---
+++ api/v1/hybridscaler_types.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"k8s.io/api/autoscaling/v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HybridScalerSpec defines the desired state of HybridScaler
+type HybridScalerSpec struct {
+	ScaleTargetRef v2.CrossVersionObjectReference `json:"scaleTargetRef"`
+	MinReplicas    *int32                         `json:"minReplicas"`
+	MaxReplicas    *int32                         `json:"maxReplicas"`
+	ResourcePolicy ResourcePolicy                 `json:"resourcePolicy"`
+}
+
+type ResourcePolicy struct {
+	MinAllowed          corev1.ResourceList    `json:"minAllowed,omitempty" protobuf:"bytes,3,rep,name=minAllowed,casttype=ResourceList,castkey=ResourceName"`
+	MaxAllowed          corev1.ResourceList    `json:"maxAllowed,omitempty" protobuf:"bytes,4,rep,name=maxAllowed,casttype=ResourceList,castkey=ResourceName"`
+	ControlledResources *[]corev1.ResourceName `json:"controlledResources,omitempty" patchStrategy:"merge" protobuf:"bytes,5,rep,name=controlledResources"`
+}
+
+// HybridScalerStatus defines the observed state of HybridScaler
+type HybridScalerStatus struct {
+	CurrentReplicas   int32 `json:"currentReplicas"`
+	CpuUtilization    int32 `json:"cpuUtilization"`
+	MemoryUtilization int32 `json:"memoryUtilization"`
+	RequestRate       int32 `json:"requestRate"`
+	ResponseTime      int32 `json:"responseTime"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// HybridScaler is the Schema for the hybridscalers API
+type HybridScaler struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HybridScalerSpec   `json:"spec,omitempty"`
+	Status HybridScalerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// HybridScalerList contains a list of HybridScaler
+type HybridScalerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HybridScaler `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&HybridScaler{}, &HybridScalerList{})
+}
